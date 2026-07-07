7c794c0868d706c8f9e3359bd1f435e8f25b2c90
meryem-cmd <tanvirmeryem2@gmail.com>
2026-07-08 02:21:36 +0500

    day 1 learning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
-
+ import Newfille  from './newfilee';
 function App() {
   
 
   return (
-    <h1>react | maryyam</h1>
+    // <h1>react | maryyam</h1>
+    <>
+    <Newfille/>
+    <h1>new react</h1>
+    </>
+
   );
 }
 
---
+++ 01vitereact/src/newfilee.jsx
@@ -0,0 +1,7 @@
+function Newfille(){
+  return(
+    <h3>this is ready</h3>
+  )
+}
+
+export default Newfille
